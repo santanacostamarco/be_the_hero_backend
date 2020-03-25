a8d37a37934e035efd2252273f1a0b460efa1168
Marco Santana <santannacostamarco@gmail.com>
2020-03-25 11:38:28 -0300

    database

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
+const OngController = require('./controllers/OngController');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        message: 'teasdst',
-    })
-});
+routes.get('/ongs', OngController.list);
+
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
