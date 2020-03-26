9c75e358ddfcd4ac3921c22428bb0d0cfa8b01d4
Marco Santana <santannacostamarco@gmail.com>
2020-03-26 11:22:06 -0300

    create session router

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,23 @@
 const express = require('express');
-
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-
 const routes = express.Router();
 
+
+const OngController = require('./controllers/OngController');
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+
+const IncidentController = require('./controllers/IncidentController');
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
 
+const SessionController = require('./controllers/SessionController');
+routes.post('/session', SessionController.create);
+
+
+const ProfileController = require('./controllers/ProfileController');
 routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
